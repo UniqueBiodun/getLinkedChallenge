aacfc94f4903ff833e2b4d832c5df591ea3cd66b
Abiodun Adu <abiodun.adu@iisysgroup.com>
2023-09-18 17:48:52 +0100

    first commit: project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ScrollToTop from "./helper/ScrollToTop";
+
+function App() {
+	return (
+		<>
+			<ScrollToTop />
+			<p className="bg-red-400">This is the Parent Component</p>
+		</>
+	);
+}
+
+export default App;
